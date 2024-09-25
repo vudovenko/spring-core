7f369920d11d75f29b58ca7dc8046ceb3a7857bc
vudovenko <vladislav.udovenko0@yandex.ru>
2024-09-25 15:23:16 +0500

    удалил лишние зависимости

--- src/main/java/vudovenko/dev/configuration/MainProperties.java
+++ src/main/java/vudovenko/dev/configuration/MainProperties.java
@@ -1,11 +1,9 @@
 package vudovenko.dev.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@EnableAspectJAutoProxy(proxyTargetClass = true)
 @PropertySource("classpath:application.properties")
 public class MainProperties {
 }
