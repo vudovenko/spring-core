aa76960d7fe9ef37a3810f52fb3022706fc85be8
vudovenko <vladislav.udovenko0@yandex.ru>
2024-11-04 02:04:05 +0500

    вынесение шаблонного кода в абстрактный класс для LeastFrequentAnswerStrategy и MostFrequentAnswerStrategy

--- src/main/java/vudovenko/dev/patterns/strategy/impl/LeastFrequentAnswerStrategy.java
+++ src/main/java/vudovenko/dev/patterns/strategy/impl/LeastFrequentAnswerStrategy.java
@@ -1,36 +1,19 @@
 package vudovenko.dev.patterns.strategy.impl;
 
-import vudovenko.dev.patterns.pollQuestion.statistic.QuestionStatistics;
-import vudovenko.dev.patterns.strategy.AnalyzeStrategy;
+import vudovenko.dev.patterns.strategy.AbstractFrequencyAnswerStrategy;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.Collection;
+import java.util.Optional;
 
-public class LeastFrequentAnswerStrategy implements AnalyzeStrategy {
+public class LeastFrequentAnswerStrategy extends AbstractFrequencyAnswerStrategy {
 
     @Override
-    public void makeAnalyze(List<QuestionStatistics> questionStatisticsList) {
-        for (QuestionStatistics questionStatistics : questionStatisticsList) {
-            int min = Integer.MAX_VALUE;
-            List<String> unpopularAnswers = new ArrayList<>();
-            for (Map.Entry<String, Integer> entry
-                    : questionStatistics.getSelectedVariantsCount().entrySet()) {
-                String variant = entry.getKey();
-                int count = entry.getValue();
-                if (count < min) {
-                    min = count;
-                    unpopularAnswers.clear();
-                    unpopularAnswers.add(variant);
-                } else if (count == min) {
-                    unpopularAnswers.add(variant);
-                }
-            }
-            System.out.printf(
-                    "Unpopular answers: %s for question: %s%n",
-                    unpopularAnswers,
-                    questionStatistics.getQuestionTitle()
-            );
-        }
+    protected Optional<Integer> findExtremeValue(Collection<Integer> values) {
+        return values.stream().min(Integer::compareTo);
+    }
+
+    @Override
+    protected String getMessage() {
+        return "Unpopular answers: %s for question: %s%n";
     }
 }
